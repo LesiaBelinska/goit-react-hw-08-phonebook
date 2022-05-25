2e7bbefb2a6a77ed425b4e441f87395564cee8c0
LesiaBelinska <lm.kudak@gmail.com>
2022-05-25 21:16:19 +0300

    Create UserMenu.jsx

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,10 @@
+
+
+export const UserMenu=(useremail) => {
+    return (
+        <div>
+            <p>Welcome {useremail}</p>
+            <button type="button"></button>
+        </div>
+    )
+}
