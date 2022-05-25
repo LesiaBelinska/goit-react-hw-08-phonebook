dcc6bf223fc8dbe775f3a0621dcd2460ceffceeb
LesiaBelinska <lm.kudak@gmail.com>
2022-05-26 00:17:31 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,9 +2,11 @@ import { Navigation } from "components/Navigation/Navigation.jsx";
 import { AuthNavigation } from "components/AuthNavigation/AuthNavigation";
 import { UserMenu } from "components/UserMenu/UserMenu";
 
+import s from "./AppBar.module.css";
+
 export const AppBar = () => {
     return (
-        <header>
+        <header className={s.header}>
             <Navigation />
             <UserMenu />
             <AuthNavigation />
