5b45db626b0c04db5744e3cd1b501e36210f18cf
LesiaBelinska <lm.kudak@gmail.com>
2022-05-27 02:15:41 +0300

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,84 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import PropTypes from 'prop-types';
+import toast from "react-hot-toast";
+import { MdClose } from "react-icons/md";
+
+//import { useGetContactByIdQuery, useUpdateContactMutation } from "redux/contactsSlice.js";
+import ContactForm from "components/ContactForm/ContactForm.jsx";
+import s from "./Modal.module.css";
+
+export const Modal = ({ onClose, id }) => {
+    
+    // const { data: contact } = useGetContactByIdQuery(id);
+    // const [updateMaterial, result] = useUpdateContactMutation();
+    
+    
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+    
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown)
+        }
+    });
+
+    
+    const handleKeyDown = e => {
+        if (e.code === 'Escape') {
+            onClose();
+        }
+    };
+    
+
+    const handleBackdropClick = e => {
+        if (e.currentTarget === e.target) {
+            onClose();
+        }
+    };
+
+    // const handleUpdateContact = async fields => {
+    //     try {
+    //         await updateMaterial({ id: id, ...fields })
+    //         onClose();
+    //     }
+    //     catch (error) {
+    //         console.log(error)
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     if (result.isSuccess) {
+    //         toast.success(`changes was saved`);
+    //     }
+    //     return
+      
+    // }, [result.isSuccess]);
+
+
+    return createPortal(
+        <div className={s.Overlay}
+            onClick={handleBackdropClick}>
+            {/* <div className={s.Modal}>
+                {contact && (
+                    <button
+                        onClick={onClose}
+                        type="button"
+                        className={s.btnClose}><MdClose className={s.icon} /></button>
+                )}
+                {contact && (
+                    <ContactForm
+                        initialValues={{ name: contact.name, phone: contact.phone }}
+                        onSubmit={handleUpdateContact}
+                        buttonText='Save' />
+                )}
+            </div> */}
+        </div>,
+        document.querySelector('#modal-root'),
+    );
+}
+
+
+Modal.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+};
