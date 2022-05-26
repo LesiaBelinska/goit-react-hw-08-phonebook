4d1f84c0355e0b3cddd1da7b8cb1f2d8ef221fcd
LesiaBelinska <lm.kudak@gmail.com>
2022-05-27 02:16:11 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,10 +1,27 @@
+import { useSelector, useDispatch } from "react-redux";
+import { FaRegUser } from "react-icons/fa";
+
+import authOperations from "redux/auth/authOperations.js";
+
+
 import s from "./UserMenu.module.css";
 
-export const UserMenu=() => {
+export const UserMenu = () => {
+    
+    const dispatch = useDispatch();
+    const name = useSelector(state => state.auth.user.name);
+
     return (
         <div className={s.menu}>
-            <p className={s.name}>Welcome</p>
-            <button type="button" className={s.button}>Sign out</button>
+            <FaRegUser />
+            <p className={s.name}>Welcome, {name}</p>
+            <button
+                type="button"
+                className={s.button}
+                onClick={() => dispatch(authOperations.logOut())}
+            >
+                Sign out
+            </button>
         </div>
-    )
+    );
 }
