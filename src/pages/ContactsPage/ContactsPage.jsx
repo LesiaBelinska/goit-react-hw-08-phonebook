3c5b471679d8c502db4dba1475b774893ab6a59b
LesiaBelinska <lm.kudak@gmail.com>
2022-05-27 18:59:33 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,15 @@
-
+//import { CreateContact } from "components/CreateContact/CreateContact.jsx";
+import { ContactList } from "components/ContactList/ContactList.jsx";
+// import { Filter } from "components/Filter/Filter.jsx";
 
 export default function ContactsPage() {
     return (
-        <>
-            <h1>CONTACTS PAGE</h1>
-        </>
-    )
-}
+        <div>
+            <h1>Phonebook</h1>
+            {/* <CreateContact /> */}
+            <h2>Contacts</h2>
+            {/* <Filter /> */}
+            <ContactList />
+        </div>
+    );
+};
