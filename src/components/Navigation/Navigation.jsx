409e0ec335c6c2324ec8295c678c7b44115ee1ec
LesiaBelinska <lm.kudak@gmail.com>
2022-05-28 01:44:32 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,13 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import s from "./Navigation.module.css";
 
 export const Navigation = () => {
+
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    
+
     return (
         <nav>
             <ul className={s.list}>
@@ -15,12 +20,13 @@ export const Navigation = () => {
                     </NavLink>
                 </li>
                 <li className={s.item}>
+                    {isLoggedIn &&
                     <NavLink
                         to="/contacts"
                         className={({ isActive }) => isActive ? `${s.activeLink}` : `${s.link}`}
                     >
                         Contacts
-                    </NavLink>
+                    </NavLink>}
                 </li>
             </ul>
         </nav>
