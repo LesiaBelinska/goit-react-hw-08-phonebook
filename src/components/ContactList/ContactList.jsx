eb4db617ee13faf71a00b92d6d5041ebca74652a
LesiaBelinska <lm.kudak@gmail.com>
2022-05-27 18:59:21 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,48 +1,26 @@
 import { useSelector } from 'react-redux';
-import { createSelector } from '@reduxjs/toolkit';
-import { useMemo } from 'react';
 
-import ContactListItem from 'components/ContactListItem/ContactListItem';
-//import { useGetContactsQuery } from 'redux/contactsSlice';
+import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import { useGetContactsQuery } from 'redux/contacts/contactsSlice';
 
 import s from './ContactList.module.css';
 
-const ContactList = () => {
-
+export const ContactList = () => {
+    const { data: contacts, isFetching} = useGetContactsQuery();
+   
     
     const filter = useSelector(state => state.filter);
 
-    // const selectFilteredContacts = useMemo(() => {
-    //     return createSelector(
-    //         [response => response.data, (_, filter) => filter],
-    //         (contacts, filter) => {
-    //             return (
-    //                 contacts?.filter(contact =>
-    //                     contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-    //                 ) ?? []
-    //             );
-    //         }
-    //     );
-    // }, []);
-
-    // const { filteredContacts, isFetching } = useGetContactsQuery(
-    //     undefined,
-    //     {
-    //         selectFromResult(result) {
-    //             return {
-    //                 ...result,
-    //                 filteredContacts: selectFilteredContacts(result, filter),
-    //             }
-    //         }
-    //     }
-    // );
+    
+    const filteredContacts = contacts?.filter(contact =>
+        contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()));
     
 
     return (
         <>
-            {/* {filteredContacts ? isFetching : <p className={s.loading}>Loading...</p>} */}
+            {contacts ? isFetching : <p className={s.loading}>Loading...</p>}
             <ul>
-                {/* {filteredContacts && filteredContacts.map(({ id, name, phone }) => {
+                {contacts && filteredContacts.map(({ id, name, phone }) => {
                     return (
                         <ContactListItem
                             key={id}
@@ -51,10 +29,77 @@ const ContactList = () => {
                             number={phone}
                         />
                     )
-                })} */}
+                })}
             </ul>
         </>
     );
 }
 
-export default ContactList;
+
+
+
+
+
+
+
+
+
+
+// import { useSelector } from 'react-redux';
+// import { createSelector } from '@reduxjs/toolkit';
+// import { useMemo } from 'react';
+
+// import { ContactListItem } from 'components/ContactListItem/ContactListItem.jsx';
+// import { useGetContactsQuery } from 'redux/contacts/contactsSlice';
+
+// import s from './ContactList.module.css';
+
+// export const ContactList = () => {
+
+    
+//     const filter = useSelector(state => state.filter);
+
+//     const selectFilteredContacts = useMemo(() => {
+//         return createSelector(
+//             [response => response.data, (_, filter) => filter],
+//             (contacts, filter) => {
+//                 return (
+//                     contacts?.filter(contact =>
+//                         contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+//                     ) ?? []
+//                 );
+//             }
+//         );
+//     }, []);
+
+//     const { filteredContacts, isFetching } = useGetContactsQuery(
+//         undefined,
+//         {
+//             selectFromResult(result) {
+//                 return {
+//                     ...result,
+//                     filteredContacts: selectFilteredContacts(result, filter),
+//                 }
+//             }
+//         }
+//     );
+    
+
+//     return (
+//         <>
+//             {filteredContacts ? isFetching : <p className={s.loading}>Loading...</p>}
+//             <ul>
+//                 {filteredContacts && filteredContacts.map(({ id, name, phone }) => {
+//                     return (
+//                         <ContactListItem
+//                             key={id}
+//                             id={id}
+//                             name={name}
+//                             number={phone}
+//                         />
+//                     )
+//                 })}
+//             </ul>
+//         </>
+//     );
+// }
