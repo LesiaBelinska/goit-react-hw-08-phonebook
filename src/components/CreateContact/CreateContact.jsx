44aaf721150f2e938a50ec0c4bbac767af3100e6
LesiaBelinska <lm.kudak@gmail.com>
2022-05-28 11:04:35 +0300

    Update CreateContact.jsx

--- src/components/CreateContact/CreateContact.jsx
+++ src/components/CreateContact/CreateContact.jsx
@@ -1,12 +1,12 @@
-import { useAddContactMutation } from 'redux/contacts/contactsSlice.js';
 import toast from 'react-hot-toast';
 
+import { useAddContactMutation } from 'redux/contacts/contactsSlice.js';
 import { ContactForm } from "components/ContactForm/ContactForm.jsx";
 
 
 export const CreateContact = () => {
 
-    const [addContact] = useAddContactMutation();
+  const [addContact] = useAddContactMutation();
     
   const handleAddContact = async (values) => {
     try {
