fb4dcc084a11417af017d55b4570f002d3a42169
LesiaBelinska <lm.kudak@gmail.com>
2022-06-04 23:19:00 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
-import { MdPhonelinkLock } from "react-icons/md";
+// import { MdPhonelinkLock } from "react-icons/md";
+import { MdOutlineMenuBook } from "react-icons/md";
+
 
 import s from "./HomePage.module.css";
 
 export default function HomePage() {
     return (
         <div className={s.container}>
-            <MdPhonelinkLock className={s.icon}/>
+            <h1 className={s.title}>Phone book</h1>
+            {/* <MdPhonelinkLock className={s.icon}/> */}
+            <MdOutlineMenuBook className={s.icon}/>
             <p className={s.text}>Please, Sign in or Sign up </p>
         </div>
     )
