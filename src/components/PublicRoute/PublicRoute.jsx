6f44a9a6c360f5d8114a48a51fc1c1a6e7cf398b
LesiaBelinska <lm.kudak@gmail.com>
2022-06-05 04:58:05 +0300

    Update PublicRoute.jsx

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 
-export const PublicRoute = ({ children, restricted=false }) => {
+export const PublicRoute = ({ children, restricted = false }) => {
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
     const shouldRedirect = isLoggedIn && restricted;
 
